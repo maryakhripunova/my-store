9ac2d6df26dbf39addf0c8a6f8846c4a34f59062
Mariya <khripunovam@gmail.com>
2019-05-05 18:06:15 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {names} from './persons';
+
+const Hello = ({name, surname}) => <p>
+  hellow <h1>{name} {surname}</h1>
+</p>
+
+const Table = ({persons}) =>
+{
+
+  let rows = names.map((ob) =>
+  <tr><td>{ob.name}</td> <td>{ob.surname}</td></tr>)
+  return (<table> 
+    <tr>
+      <th>name</th>
+      <th>surname</th>
+      </tr>
+      {rows} </table>)
+}
 
 function App() {
+  let name = names.map((ob) =>
+  <Hello name = {ob.name} surname = {ob.surname}/>)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Table persons = {names} />     
+        
+
       </header>
     </div>
   );
---
+++ src/persons.js
@@ -0,0 +1,5 @@
+export const names = [
+    { name: "Masha", surname: "Khripunova" },
+    { name: "Katya", surname: "Khripunova" },
+    { name: "Oleg", surname: "Nizhnikov" }];
+
