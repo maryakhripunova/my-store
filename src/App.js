5d89bf9c9779bbae8a8c027c720e398636d28a0d
Mariya <khripunovam@gmail.com>
2019-05-05 18:35:21 +0300

    button choose

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {names} from './persons';
+import { names } from './persons';
 
-const Hello = ({name, surname}) => <p>
+const Hello = ({ name, surname }) => <p>
   hellow <h1>{name} {surname}</h1>
 </p>
 
-const Table = ({persons}) =>
-{
+const Table = ({ persons }) => {
 
-  let rows = names.map((ob) =>
-  <tr><td>{ob.name}</td> <td>{ob.surname}</td></tr>)
-  return (<table> 
+  let rows = names.map((ob, i) =>
+    <tr>
+      <td>{ob.name}</td>
+      <td>{ob.surname}</td>
+      <td><button onClick={() => choose(i)}>choose</button></td>
+    </tr>
+  )
+  let choose = (i) => alert("chosen " + i)
+
+  return (<table>
     <tr>
       <th>name</th>
       <th>surname</th>
-      </tr>
-      {rows} </table>)
+      <th>say Hellow</th>
+    </tr>
+    {rows} </table>)
 }
 
-function App() {
-  let name = names.map((ob) =>
-  <Hello name = {ob.name} surname = {ob.surname}/>)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Table persons = {names} />     
-        
+class App extends React.Component {
+  render() {
+    let name = names.map((ob) =>
+      <Hello name={ob.name} surname={ob.surname} />)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Table persons={names} />
 
-      </header>
-    </div>
-  );
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
